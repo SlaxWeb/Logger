b3c4a0664f8d239796f3fe7cdc11da9d51b2cd83
Tomaz Lovrec <tomaz.lovrec@gmail.com>
2016-02-22 16:53:47 +0100

    create new structure for the logger component

---
+++ src/Factory.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Logger Factory
+ *
+ * Instantiate the Monolog\Logger, and return an instance to the caller.
+ *
+ * @package   SlaxWeb\Logger
+ * @author    Tomaz Lovrec <tomaz.lovrec@gmail.com>
+ * @copyright 2016 (c) Tomaz Lovrec
+ * @license   MIT <https://opensource.org/licenses/MIT>
+ * @link      https://github.com/slaxweb/
+ * @version   0.2
+ */
+namespace SlaxWeb\Logger;
+
+class Factory
+{
+}
---
+++ src/Service/Provider.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Logger Service Provider
+ *
+ * Register the logger and its handler as services.
+ *
+ * @package   SlaxWeb\Logger
+ * @author    Tomaz Lovrec <tomaz.lovrec@gmail.com>
+ * @copyright 2016 (c) Tomaz Lovrec
+ * @license   MIT <https://opensource.org/licenses/MIT>
+ * @link      https://github.com/slaxweb/
+ * @version   0.2
+ */
+namespace SlaxWeb\Logger\Service;
+
+class Provider
+{
+}
